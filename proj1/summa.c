f2a19678be323aeec6c8b38f2942e8bbe4d887ea
Rodolfo Carvajal <rodolfo.carvajal@gmail.com>
2011-09-14 11:53:02 -0400

    Most of the communication done. First try.
    I just broadcast an int in every row for now.

--- proj1/summa.c
+++ proj1/summa.c
@@ -37,10 +37,62 @@
  *  pb is the Panel Block Size
  **/
 void summa(int m, int n, int k, double *Ablock, double *Bblock, double *Cblock,
-		int procGridX, int procGridY, int pb) {
+        int procGridX, int procGridY, int pb) {
+
+    int i;
+    int p;
+    int rank = 0;
+    int indexX = rank % procGridX;
+    int indexY = (rank - indexX) / procGridX;
+    int rowGroupIndex[procGridY];
+    int colGroupIndex[procGridX];
+    int whoseTurnRow;
+    int whoseTurnCol;
+
+    MPI_Group originalGroup, rowGroup, colGroup;
+    MPI_Comm rowComm, colComm;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_group(MPI_COMM_WORLD, &originalGroup);
+
+    for(p = 0; p < procGridY; ++p)
+        rowGroupIndex[p] = p * procGridX + indexX;
+
+    for(p = 0; p < procGridX; ++p)
+        colGroupIndex[p] = indexY * procGridX + p;
+
+    /* Create groups */
+    MPI_Group_incl(originalGroup, procGridY, rowGroupIndex, &rowGroup);
+    MPI_Group_incl(originalGroup, procGridX, colGroupIndex, &colGroup);
+
+    /* Create communicators */
+    MPI_Comm_create(MPI_COMM_WORLD, rowGroup, &rowComm);
+    MPI_Comm_create(MPI_COMM_WORLD, colGroup, &colComm);
+
+    for(i = 0; i < k/pb; ++i)
+    {
+        whoseTurnRow = (int) i * pb * procGridX / k;
+        whoseTurnCol = (int) i * pb * procGridY / k;
 
 
+        /* Row */
+        if(rank == whoseTurnRow)
+        {
+            int buffer = 1;
+            MPI_Bcast(&buffer, sizeof(int), MPI_INT, whoseTurnRow, rowComm);
 
-	MPI_Barrier(MPI_COMM_WORLD);
+            printf("I'm proc: %d, and sent message!\n", rank);
+        }
+        else
+        {
+            int buffer;
+            MPI_Bcast(&buffer, sizeof(int), MPI_INT, whoseTurnRow, rowComm);
+            printf("I'm proc: %d, and got message: %d\n", rank, buffer);
+
+        }
+
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
 
 }
